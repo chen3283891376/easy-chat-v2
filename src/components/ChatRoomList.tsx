ed6ba78e73cfdb1de08507d8a326d7f847a9ebae
Chenify <3283891376@qq.com>
2026-06-27 14:19:26 +0800

    feat: 改房间名功能

--- src/components/ChatRoomList.tsx
+++ src/components/ChatRoomList.tsx
@@ -1,7 +1,6 @@
-import { DeleteIcon } from 'lucide-react';
+import { DeleteIcon, EditIcon } from 'lucide-react';
 import { Button } from '../components/ui/button';
 import { ScrollArea } from '../components/ui/scroll-area';
-import { useChat } from '../context/ChatContext';
 import {
     ContextMenu,
     ContextMenuContent,
@@ -9,11 +8,52 @@ import {
     ContextMenuItem,
     ContextMenuTrigger,
 } from './ui/context-menu';
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle } from './ui/dialog';
+import { Field } from './ui/field';
+import { Label } from './ui/label';
+import { Input } from './ui/input';
+import { useRoom } from '@/context/room-context';
+import { useState } from 'react';
 
 export function ChatRoomList() {
-    const { roomList, currentRoom, switchToRoom, setRoomList } = useChat();
+    const {
+        roomList,
+        currentRoom,
+        switchToRoom,
+        setRoomList,
+        editingRoomName,
+        setEditingRoomName,
+        setEditingRoom,
+        editRoomName,
+    } = useRoom();
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <ScrollArea className="h-[65vh] px-1 border rounded">
+            <Dialog open={isOpen} onOpenChange={setIsOpen}>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>编辑房间名</DialogTitle>
+                    </DialogHeader>
+                    <Field>
+                        <Label>房间名</Label>
+                        <Input value={editingRoomName} onChange={e => setEditingRoomName(e.target.value)} />
+                    </Field>
+                    <DialogFooter>
+                        <DialogClose asChild>
+                            <Button variant="outline">取消</Button>
+                        </DialogClose>
+                        <Button
+                            type="submit"
+                            onClick={async () => {
+                                await editRoomName();
+                                setIsOpen(false);
+                            }}
+                        >
+                            保存
+                        </Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
             <div className="space-y-1">
                 {roomList.map(room => (
                     <ContextMenu>
@@ -29,6 +69,16 @@ export function ChatRoomList() {
                         </ContextMenuTrigger>
                         <ContextMenuContent side="right">
                             <ContextMenuGroup>
+                                <ContextMenuItem
+                                    onClick={() => {
+                                        setEditingRoomName(room.name);
+                                        setEditingRoom(room);
+                                        setIsOpen(true);
+                                    }}
+                                >
+                                    <EditIcon />
+                                    编辑
+                                </ContextMenuItem>
                                 <ContextMenuItem
                                     variant="destructive"
                                     onClick={() => {
